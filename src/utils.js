57691edeee598e845fd489b8845db6945a22f1c2
shichuanzhi <shichuanzhi@tal.com>
2020-12-09 14:23:32 +0800

    优化

--- src/utils.js
+++ src/utils.js
@@ -24,11 +24,6 @@ const deletePrefix = (str, prefix) => {
   return str.replace(reg, '')
 }
 
-// 删除后缀
-const deleteSuffix = (str, suffix) => {
-  return str.replace(suffix, '')
-}
-
 const JSON2AST = (JSON = {}) => {
 
   if (isAST(JSON) || isComponent(JSON)) return JSON
@@ -47,42 +42,54 @@ const JSON2AST = (JSON = {}) => {
     text
   }
 
-  const handleBind = (key, value) => {
+  const bind = (key, value) => {
     key = deletePrefix(key, ':')
-    AST.data.props = assign({}, AST.data.props, { [key]: value }) // v-bind放到props里，兼容AST.data.props为undefined的情况
+    AST.data.props = assign({}, AST.data.props, { [key]: value })
   }
 
-  const handleNativeOn = (key, value) => {
-    key = deleteSuffix(key, '.native')
-    AST.data.nativeOn = assign({}, AST.data.nativeOn, { [key]: value })
-  }
-
-  const handleCommonOn = (key, value) => {
-    AST.data.on = assign({}, AST.data.on, { [key]: value })
-  }
-
-  const handleOn = (key, value) => {
+  const on = (key, value) => {
     key = deletePrefix(key, '@')
+
     const fn = R.cond([
-      [key => key.includes('.native'), handleNativeOn],
-      [R.T,                            handleCommonOn]
+      [
+        key => key.includes('.native'), 
+        (key, value) => {
+          key = key.split('.').shift()
+          AST.data.nativeOn = assign({}, AST.data.nativeOn, { [key]: value })
+        }
+      ],
+      [
+        R.T,
+        (key, value) => {
+          AST.data.on = assign({}, AST.data.on, { [key]: value })
+        }
+      ]
     ])
+
     fn(key, value)
   }
 
-  const handleCommon = (key, value) => {
-    AST.data[key] = value
-  }
-
-  const handleOthers = (key, value) => {
-    AST.data.attrs = assign({}, AST.data.attrs, { [key]: value })
-  }
-
   const fn = R.cond([
-    [key => key.startsWith(':'),                             handleBind],
-    [key => key.startsWith('@'),                             handleOn],
-    [key => ['class', 'style', 'slot', 'ref'].includes(key), handleCommon],
-    [R.T,                                                    handleOthers]
+    [
+      key => ['class', 'style', 'slot', 'ref'].includes(key),
+      (key, value) => {
+        AST.data[key] = value
+      }
+    ],
+    [
+      key => key.startsWith(':'),
+      bind
+    ],
+    [
+      key => key.startsWith('@'),
+      on
+    ],
+    [
+      R.T,
+      (key, value) => {
+        AST.data.attrs = assign({}, AST.data.attrs, { [key]: value })
+      }
+    ]
   ])
 
   for (let [key, value] of entries(attrs)) {
@@ -99,17 +106,24 @@ const renderMixin = {
     const handler = ({ tag, data, children = [] } = {}) => {
 
       const fn = R.cond([
-        [child => child.tag,                      handler],
-        [child => isComponent(child),             child => handler({ tag: child })],
-        [child => child.text && !child.isComment, child => child.text]
+        [
+          child => child.tag,
+          handler
+        ],
+        [
+          child => isComponent(child),
+          child => handler({ tag: child })
+        ],
+        [
+          child => child.text && !child.isComment,
+          child => child.text
+        ]
       ])
 
       return h(tag, data, children.map(fn))
     }
 
-    const AST = JSON2AST(this.JSON)
-
-    return handler(AST)
+    return handler(JSON2AST(this.JSON))
   }
 }
 
